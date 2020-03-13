2d589660c468080b58d0b886d9e56b0f80432812
Leonardo Cannarella <leonardocannarella11@gmail.com>
2020-03-13 12:12:14 +0100

    Add files via upload
    - Aggiunt: JCheckBox e JRadioButton

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -2,12 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class MyPanel extends JPanel implements ActionListener
+public class MyPanel extends JPanel implements ActionListener, ItemListener
 {
     JLabel lblInfo;
     JButton btnProva, btnProva2;
+    JCheckBox ck1;
     JTextField text1, text2;
+    JRadioButton b1, b2;
+    ButtonGroup grp;
 
     public MyPanel()
     {
@@ -17,16 +22,42 @@ public class MyPanel extends JPanel implements ActionListener
         text1 = new JTextField("Scrivi qui");
         text2 = new JTextField("Non puoi modificarmi.");
         text2.setEditable(false);
+        ck1 = new JCheckBox("Cambia sfondo");
+        b1 = new JRadioButton("Arancione");
+        b2 = new JRadioButton("Rosa");
+        grp = new ButtonGroup();
+        grp.add(b1);
+        grp.add(b2);
 
-        setLayout(new GridLayout(6,1));
+        setLayout(new GridLayout(8,1));
         add(lblInfo);
         add(text1);
         add(text2);
         add(btnProva);
         add(btnProva2);
+        add(ck1);
+        add(b1);
+        add(b2);
 
         btnProva.addActionListener(this);
         btnProva2.addActionListener(this);
+        ck1.addItemListener(this);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+    }
+
+    public void itemStateChanged(ItemEvent e)
+    {
+        if(ck1.isSelected())
+        {
+            setBackground(Color.BLUE);
+            text2.setText("Opzione attivata.");
+        }
+        else
+        {
+            setBackground(Color.white);
+            text2.setText("Opzione disattivata.");
+        }
     }
 
     public void actionPerformed(ActionEvent e)
@@ -42,6 +73,16 @@ public class MyPanel extends JPanel implements ActionListener
         {
             lblInfo.setText("Sfondo impostato su ROSSO.");
             setBackground(Color.red);
+        }
+        if(pulsantePremuto==b1)
+        {
+            text2.setText("Radio button cliccato - ARANCIONE!");
+            setBackground(Color.orange);
+        }
+        if(pulsantePremuto==b2)
+        {
+            text2.setText("Radio button cliccato - ROSA!");
+            setBackground(Color.pink);
         }
     }
 }
